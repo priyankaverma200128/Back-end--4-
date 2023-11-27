f512ee542d22f803378280b25c1be01bca5514fe
Priyanka200128 <priyanka28vr@gmail.com>
2023-11-27 05:00:19 -0500

    db changed

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1/project1')
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
     console.log("DB Connected");
 })
