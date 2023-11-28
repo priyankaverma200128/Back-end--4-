4e1fe17c60bae13f526c2a88ed892a1c56a0aab1
Priyanka200128 <priyanka28vr@gmail.com>
2023-11-28 03:56:17 -0500

    s3 defined

--- Server/utilites/helper.js
+++ Server/utilites/helper.js
@@ -4,7 +4,14 @@ const path = require('path')
 const { S3Client,GetObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
-
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    sessionToken: process.env.AWS_SESSION_TOKEN
+  },
+  region: process.env.AWS_REGION,
+})
 
 async function generatePresignedUrl (bucket,key,expiresIn = 3600){
     const command = new GetObjectCommand({
