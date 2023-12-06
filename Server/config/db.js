a178d5e48a3c03a859b3efbe923106ada32f5dd4
Priyanka200128 <priyanka28vr@gmail.com>
2023-12-06 04:07:27 -0500

    bd rectified

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI.then(()=>{
+mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("DB Connected");
 })
 .catch((err)=>{
